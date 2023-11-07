53f30509d0db8f5d3d30eeb671af968e79e24bc6
AnOldStory <hc9904@naver.com>
2023-11-08 06:02:59 +0900

    Create Temp Api Server

--- packages/backend/lib/backend.js
+++ packages/backend/lib/backend.js
@@ -1,7 +1,17 @@
-'use strict';
+// "use strict";
 
-module.exports = backend;
+// module.exports = backend;
 
-function backend() {
-  return 'Hello from backend';
-}
+// function backend() {
+//   return "Hello from backend";
+// }
+
+var http = require("http");
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  var message = "It works!\n",
+    version = "NodeJS " + process.versions.node + "\n",
+    response = [message, version].join("\n");
+  res.end(response);
+});
+server.listen();
